716a6cbe3c4f6b433417bbbb6f1ff5b1ce14b3e0
Alvaro Sanz Rodrigo <alvaro.sanz@live.u-tad.com>
2018-04-05 10:28:38 +0200

    Hito 1 finalizado

---
+++ primerHitoSwift/TVCMiCelda1.swift
@@ -0,0 +1,25 @@
+//
+//  TVCMiCelda1.swift
+//  primerHitoSwift
+//
+//  Created by Álvaro Sanz Rodrigo on 5/4/18.
+//  Copyright © 2018 Alvaro Sanz Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class TVCMiCelda1: UITableViewCell {
+
+    @IBOutlet weak var miLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ primerHitoSwift/VCItem1Table.swift
@@ -0,0 +1,61 @@
+//
+//  VCItem1Table.swift
+//  primerHitoSwift
+//
+//  Created by Álvaro Sanz Rodrigo on 5/4/18.
+//  Copyright © 2018 Alvaro Sanz Rodrigo. All rights reserved.
+//
+
+import UIKit
+
+class VCItem1Table: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    @IBOutlet weak var miTabla: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let miCelda1 = tableView.dequeueReusableCell(withIdentifier: "miCelda1") as! TVCMiCelda1
+        
+        if indexPath.row == 0{
+            miCelda1.miLabel?.text = "Sanz"
+        } else if indexPath.row == 1 {
+            miCelda1.miLabel?.text = "Pablo"
+        }else if indexPath.row == 2 {
+            miCelda1.miLabel?.text = "Yony"
+        }else if indexPath.row == 3 {
+            miCelda1.miLabel?.text = "Joaquin"
+        }
+        return miCelda1
+    }
+    
+    
+
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
